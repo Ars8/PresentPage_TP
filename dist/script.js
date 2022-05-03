fa2b76463ab7cd95e756fe8f1c95ddb5a1c61dcb
Ars8 <pogosov-91@mail.ru>
2022-05-03 10:50:42 +0300

    adaptive done

--- dist/script.js
+++ dist/script.js
@@ -187,25 +187,22 @@ const color = () => {
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 const move = () => {
-  const plan = document.querySelector('#plan');
-  const fact = document.querySelector('#fact');
-  const elem = document.querySelector('#myBar');
+  const plan = document.querySelector("#plan");
+  const fact = document.querySelector("#fact");
+  const elem = document.querySelector("#myBar");
 
   function calc() {
     if (+fact.value > +plan.value) {
       elem.style.width = 0 + "%";
-      alert('Слишком много!');
-      fact.value = '';
+      alert("Слишком много!");
+      fact.value = "";
     } else {
       let factPercent = fact.value / plan.value * 100;
       elem.style.width = factPercent + "%";
     }
   }
 
-  fact.addEventListener('input', calc);
-  console.log(plan.value);
-  console.log(fact.value);
-  console.log(elem);
+  fact.addEventListener("input", calc);
 };
 
 /* harmony default export */ __webpack_exports__["default"] = (move);
--- src/js/modules/progress.js
+++ src/js/modules/progress.js
@@ -1,21 +1,19 @@
 const move = () => {
-  const plan = document.querySelector('#plan');
-  const fact = document.querySelector('#fact');
-  const elem = document.querySelector('#myBar');
-    
-function calc() {
-  if (+fact.value > +plan.value) {
-    elem.style.width = 0 + "%";
-    alert('Слишком много!');
-    fact.value = '';
-  } else {
-    let factPercent = (fact.value / plan.value)*100;
-    elem.style.width = factPercent + "%";
+  const plan = document.querySelector("#plan");
+  const fact = document.querySelector("#fact");
+  const elem = document.querySelector("#myBar");
+
+  function calc() {
+    if (+fact.value > +plan.value) {
+      elem.style.width = 0 + "%";
+      alert("Слишком много!");
+      fact.value = "";
+    } else {
+      let factPercent = (fact.value / plan.value) * 100;
+      elem.style.width = factPercent + "%";
+    }
   }
-}
-fact.addEventListener('input', calc);
-  console.log(plan.value)
-  console.log(fact.value)
-  console.log(elem)
-}
+
+  fact.addEventListener("input", calc);
+};
 export default move;
